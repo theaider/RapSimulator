c755d5f5958b36c8f66742bf16d5055a23931ba0
theaider <80531074+theaider@users.noreply.github.com>
2026-05-14 09:13:52 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import RapSimulatorPrototype from "../rap_simulator_playable";
+import RapSimulatorPrototype from "./rap_simulator_playable.jsx";
 
 createRoot(document.getElementById("root")).render(<RapSimulatorPrototype />);
